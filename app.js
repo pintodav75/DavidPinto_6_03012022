142874127f7807f7eab9396596805ce2a45772b0
david <da.pinto75@gmail.com>
2022-01-05 23:44:24 +0100

    second push

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const req = require('express/lib/request');
 const res = require('express/lib/response');
 const app = express();
 const mongoose = require('mongoose');
+
+/* ROUTES */
+const userRoutes = require('./routes/user');
+
  /*connection base de donnees Mongodb */
 mongoose.connect('mongodb+srv://dpinto75:qwerty93@cluster0.abjfj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -11,13 +15,15 @@ mongoose.connect('mongodb+srv://dpinto75:qwerty93@cluster0.abjfj.mongodb.net/myF
   .then(() => console.log('Connexion à MongoDB reussi !'))
   .catch(() => console.log('Connexion à MongoDB echoue !'));
 
-  app.use(express.json());
-
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+  app.use(express.json());
+
+  app.use('/api/auth', userRoutes);
 
   module.exports = app;
