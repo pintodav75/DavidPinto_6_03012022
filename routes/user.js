f0a1333516e09909957fc5f7874081d9bbc8fc17
david <da.pinto75@gmail.com>
2022-01-14 17:55:14 +0100

    verif de MDP

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl  = require('../controllers/user');
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const check_password = require('../middleware/check_password');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', check_password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
